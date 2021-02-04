e0ebfea04655dc837a29a1a019ab7f19b1b002c0
Giuseppe Barbieri <elect86@gmail.com>
2021-02-04 21:41:05 +0100

    nico

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 }
 
 group = "com.github.elect86"
-version = "0.0.4"
+version = "0.0.5"
 
 repositories {
     // Use jcenter for resolving dependencies.
@@ -52,7 +52,7 @@ gradlePlugin {
     // Define the plugin
     plugins.create("sciJava") {
         //        id = "sciJava.greeting"
-        id = "com.github.elect86.sciJava"
+        id = "sciJava"
         displayName = "sciJava plugin"
         description = "plugin to easier version alignment for sciJava in Gradle"
         implementationClass = "sciJava.SciJavaPlugin"
